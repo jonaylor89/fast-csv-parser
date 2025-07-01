4d291694139b59fbba4800ac1b70cceb895429a5
Johannes Naylor <jonaylor89@gmail.com>
2025-07-01 12:12:07 +0200

    CI

--- __test__/test.mjs
+++ __test__/test.mjs
@@ -1,83 +1,74 @@
-import path from "path";
-
 import test from "ava";
-import csv from "../index.js";
-
-import bops from "bops";
-import spectrum from "csv-spectrum";
-import concat from "concat-stream";
-import execa from "execa";
-
 import { collect } from "./helpers/helper.mjs";
+import csv from "../csv-parser.js";
+import { Buffer } from "buffer";
 
 const eol = "\n";
 
-test.cb("simple csv", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    t.snapshot(lines[0], "first row");
-    t.is(lines.length, 1, "1 row");
-    t.end();
-  };
+test("simple csv", async (t) => {
+  const { error, lines } = await collect("basic");
 
-  collect("basic", verify);
+  t.false(error, "no err");
+  t.snapshot(lines[0], "first row");
+  t.is(lines.length, 1, "1 row");
 });
 
-test.cb("supports strings", (t) => {
+test("supports strings", async (t) => {
   const parser = csv();
+  const lines = [];
 
-  parser.on("data", (data) => {
-    t.snapshot(data);
-    t.end();
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
   });
 
-  parser.write("hello\n");
-  parser.write("world\n");
+  parser.write(Buffer.from("hello\n"));
+  parser.write(Buffer.from("world\n"));
   parser.end();
+
+  await promise;
+
+  t.snapshot(lines[0]);
+  t.is(lines.length, 1);
 });
 
-test.cb("newlines in a cell", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    t.snapshot(lines[0], "first row");
-    t.snapshot(lines[1], "second row");
-    t.snapshot(lines[2], "fourth row");
-    t.is(lines.length, 3, "3 rows");
-    t.end();
-  };
+test("newlines in a cell", async (t) => {
+  const { error, lines } = await collect("newlines");
 
-  collect("newlines", verify);
+  t.false(error, "no err");
+  t.snapshot(lines[0], "first row");
+  t.snapshot(lines[1], "second row");
+  t.snapshot(lines[2], "fourth row");
+  t.is(lines.length, 3, "3 rows");
 });
 
-test.cb("raw escaped quotes", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    t.snapshot(lines[0], "first row");
-    t.snapshot(lines[1], "second row");
-    t.snapshot(lines[2], "third row");
-    t.is(lines.length, 3, "3 rows");
-    t.end();
-  };
+test("raw escaped quotes", async (t) => {
+  const { error, lines } = await collect("escape-quotes");
 
-  collect("escape-quotes", verify);
+  t.false(error, "no err");
+  t.snapshot(lines[0], "first row");
+  t.snapshot(lines[1], "second row");
+  t.snapshot(lines[2], "third row");
+  t.is(lines.length, 3, "3 rows");
 });
 
-test.cb("raw escaped quotes and newlines", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    t.snapshot(lines[0], "first row");
-    t.snapshot(lines[1], "second row");
-    t.snapshot(lines[2], "third row");
-    t.is(lines.length, 3, "3 rows");
-    t.end();
-  };
+test("raw escaped quotes and newlines", async (t) => {
+  const { error, lines } = await collect("quotes+newlines");
 
-  collect("quotes+newlines", verify);
+  t.false(error, "no err");
+  t.snapshot(lines[0], "first row");
+  t.snapshot(lines[1], "second row");
+  t.snapshot(lines[2], "third row");
+  t.is(lines.length, 3, "3 rows");
 });
 
-test.cb("line with comma in quotes", (t) => {
-  const headers = bops.from("a,b,c,d,e\n");
-  const line = bops.from('John,Doe,120 any st.,"Anytown, WW",08123\n');
+test("line with comma in quotes", async (t) => {
+  const headers = Buffer.from("a,b,c,d,e\n");
+  const line = Buffer.from('John,Doe,120 any st.,"Anytown, WW",08123\n');
   const correct = JSON.stringify({
     a: "John",
     b: "Doe",
@@ -85,176 +76,198 @@ test.cb("line with comma in quotes", (t) => {
     d: "Anytown, WW",
     e: "08123",
   });
+
   const parser = csv();
+  const lines = [];
+
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
+  });
 
   parser.write(headers);
   parser.write(line);
   parser.end();
 
-  parser.once("data", (data) => {
-    t.is(JSON.stringify(data), correct);
-    t.end();
-  });
+  await promise;
+
+  t.is(JSON.stringify(lines[0]), correct);
 });
 
-test.cb("line with newline in quotes", (t) => {
-  const headers = bops.from("a,b,c\n");
-  const line = bops.from(`1,"ha ${eol}""ha"" ${eol}ha",3\n`);
+test("line with newline in quotes", async (t) => {
+  const headers = Buffer.from("a,b,c\n");
+  const line = Buffer.from(`1,"ha ${eol}""ha"" ${eol}ha",3\n`);
   const correct = JSON.stringify({
     a: "1",
     b: `ha ${eol}"ha" ${eol}ha`,
     c: "3",
   });
+
   const parser = csv();
+  const lines = [];
+
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
+  });
 
   parser.write(headers);
   parser.write(line);
   parser.end();
 
-  parser.once("data", (data) => {
-    t.is(JSON.stringify(data), correct);
-    t.end();
-  });
+  await promise;
+
+  t.is(JSON.stringify(lines[0]), correct);
 });
 
-test.cb("cell with comma in quotes", (t) => {
-  const headers = bops.from("a\n");
-  const cell = bops.from('"Anytown, WW"\n');
+test("cell with comma in quotes", async (t) => {
+  const headers = Buffer.from("a\n");
+  const cell = Buffer.from('"Anytown, WW"\n');
   const correct = "Anytown, WW";
+
   const parser = csv();
+  const lines = [];
 
-  parser.write(headers);
-  parser.write(cell);
-  parser.end();
-
-  parser.once("data", (data) => {
-    t.is(data.a, correct);
-    t.end();
-  });
-});
-
-test.cb("cell with newline", (t) => {
-  const headers = bops.from("a\n");
-  const cell = bops.from(`"why ${eol}hello ${eol}there"\n`);
-  const correct = `why ${eol}hello ${eol}there`;
-  const parser = csv();
-
-  parser.write(headers);
-  parser.write(cell);
-  parser.end();
-
-  parser.once("data", (data) => {
-    t.is(data.a, correct);
-    t.end();
-  });
-});
-
-test.cb("cell with escaped quote in quotes", (t) => {
-  const headers = bops.from("a\n");
-  const cell = bops.from('"ha ""ha"" ha"\n');
-  const correct = 'ha "ha" ha';
-  const parser = csv();
-
-  parser.write(headers);
-  parser.write(cell);
-  parser.end();
-
-  parser.once("data", (data) => {
-    t.is(data.a, correct);
-    t.end();
-  });
-});
-
-test.cb("cell with multibyte character", (t) => {
-  const headers = bops.from("a\n");
-  const cell = bops.from("this ʤ is multibyte\n");
-  const correct = "this ʤ is multibyte";
-  const parser = csv();
-
-  parser.write(headers);
-  parser.write(cell);
-  parser.end();
-
-  parser.once("data", (data) => {
-    t.is(data.a, correct, "multibyte character is preserved");
-    t.end();
-  });
-});
-
-test.cb("geojson", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    const lineObj = {
-      type: "LineString",
-      coordinates: [
-        [102.0, 0.0],
-        [103.0, 1.0],
-        [104.0, 0.0],
-        [105.0, 1.0],
-      ],
-    };
-    t.deepEqual(JSON.parse(lines[1].geojson), lineObj, "linestrings match");
-    t.end();
-  };
-
-  collect("geojson", verify);
-});
-
-test.cb("empty columns", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    function testLine(row) {
-      t.is(Object.keys(row).length, 3, "Split into three columns");
-      t.truthy(/^2007-01-0\d$/.test(row.a), "First column is a date");
-      t.truthy(row.b !== undefined, "Empty column is in line");
-      t.is(row.b.length, 0, "Empty column is empty");
-      t.truthy(row.c !== undefined, "Empty column is in line");
-      t.is(row.c.length, 0, "Empty column is empty");
-    }
-    lines.forEach(testLine);
-    t.end();
-  };
-
-  collect("empty-columns", ["a", "b", "c"], verify);
-});
-
-test.cb("csv-spectrum", (t) => {
-  spectrum((err, data) => {
-    if (err) throw err;
-    let pending = data.length;
-    data.map((d) => {
-      const parser = csv();
-      const collector = concat((objs) => {
-        t.snapshot(objs, d.name);
-        done();
-      });
-      parser.pipe(collector);
-      parser.write(d.csv);
-      parser.end();
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
     });
-    function done() {
-      pending--;
-      if (pending === 0) t.end();
-    }
+    parser.on("end", () => {
+      resolve();
+    });
   });
+
+  parser.write(headers);
+  parser.write(cell);
+  parser.end();
+
+  await promise;
+
+  t.is(lines[0].a, correct);
 });
 
-test.cb("process all rows", (t) => {
-  const verify = (err, lines) => {
-    t.false(err, "no err");
-    t.is(lines.length, 7268, "7268 rows");
-    t.end();
+test("cell with newline", async (t) => {
+  const headers = Buffer.from("a\n");
+  const cell = Buffer.from(`"why ${eol}hello ${eol}there"\n`);
+  const correct = `why ${eol}hello ${eol}there`;
+
+  const parser = csv();
+  const lines = [];
+
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
+  });
+
+  parser.write(headers);
+  parser.write(cell);
+  parser.end();
+
+  await promise;
+
+  t.is(lines[0].a, correct);
+});
+
+test("cell with escaped quote in quotes", async (t) => {
+  const headers = Buffer.from("a\n");
+  const cell = Buffer.from('"ha ""ha"" ha"\n');
+  const correct = 'ha "ha" ha';
+
+  const parser = csv();
+  const lines = [];
+
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
+  });
+
+  parser.write(headers);
+  parser.write(cell);
+  parser.end();
+
+  await promise;
+
+  t.is(lines[0].a, correct);
+});
+
+test("cell with multibyte character", async (t) => {
+  const headers = Buffer.from("a\n");
+  const cell = Buffer.from("this ʤ is multibyte\n");
+  const correct = "this ʤ is multibyte";
+
+  const parser = csv();
+  const lines = [];
+
+  const promise = new Promise((resolve) => {
+    parser.on("data", (data) => {
+      lines.push(data);
+    });
+    parser.on("end", () => {
+      resolve();
+    });
+  });
+
+  parser.write(headers);
+  parser.write(cell);
+  parser.end();
+
+  await promise;
+
+  t.is(lines[0].a, correct, "multibyte character is preserved");
+});
+
+test("geojson", async (t) => {
+  const { error, lines } = await collect("geojson");
+
+  t.false(error, "no err");
+  const lineObj = {
+    type: "LineString",
+    coordinates: [
+      [102.0, 0.0],
+      [103.0, 1.0],
+      [104.0, 0.0],
+      [105.0, 1.0],
+    ],
   };
-
-  collect("large-dataset", {}, verify);
+  t.deepEqual(JSON.parse(lines[1].geojson), lineObj, "linestrings match");
 });
 
-test("binary stanity", async (t) => {
-  const binPath = path.resolve(__dirname, "../bin/csv-parser");
-  const { stdout } = await execa(
-    `echo "a\n1" | ${process.execPath} ${binPath}`,
-    { shell: true },
-  );
+test("empty columns", async (t) => {
+  const { error, lines } = await collect("empty-columns", ["a", "b", "c"]);
 
-  t.snapshot(stdout);
+  t.false(error, "no err");
+
+  function testLine(row) {
+    t.is(Object.keys(row).length, 3, "Split into three columns");
+    t.truthy(/^2007-01-0\d$/.test(row.a), "First column is a date");
+    t.truthy(row.b !== undefined, "Empty column is in line");
+    t.is(row.b.length, 0, "Empty column is empty");
+    t.truthy(row.c !== undefined, "Empty column is in line");
+    t.is(row.c.length, 0, "Empty column is empty");
+  }
+
+  lines.forEach(testLine);
+});
+
+test("process all rows", async (t) => {
+  const { error, lines } = await collect("large-dataset", {});
+
+  t.false(error, "no err");
+  t.is(lines.length, 7268, "7268 rows");
 });
